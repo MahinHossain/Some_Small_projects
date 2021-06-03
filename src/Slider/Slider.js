bc10c03c23685c1d25002407e384264079793f8a
Md. Mahin Hossain <intern67.it@akij.net>
2021-06-03 18:04:08 +0600

    m

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -8,22 +8,24 @@ export default function Slider() {
       </h4>
 
       <div class="card text-left rounded">
-        <div class="row clearfix">
+        <div class="row ">
           <div className="col-4"></div>
           <div className="col-4">
             {" "}
             <img
-              class="cardimage "
+              class="cardimage clearfix"
               src="https://images.pexels.com/photos/1553463/pexels-photo-1553463.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
               alt=""
             />
           </div>
         </div>
 
-        <div class="card-body row spaceb clearfix h-25">
+        <div class="card-body row spaceb  h-25">
+          <div className="col-2 clearfix ">
+            <button className="btn btn-success">{}</button>
+          </div>
+          <div className="col-8 align-self-center">smdam dmadasm </div>
           <div className="col-2 ">1</div>
-          <div className="col-8">smdam dmadasm </div>
-          <div className="col-2">1</div>
         </div>
       </div>
     </div>
